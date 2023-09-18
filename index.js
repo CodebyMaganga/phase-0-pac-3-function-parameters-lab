393bd1e3596f0f3464f0ceac550f662b1de29530
Maganga Mwambonu <codebymaganga@gmail.com>
2023-09-18 11:41:59 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name){
+    return `Hi, my name is ${name}`
+}
+console.log(introduction("Samuel"))
+
+function introductionWithLanguage(name, language){
+    return `Hi my name is ${name} and I am learning to program in ${language}`
+}
+console.log(introductionWithLanguage("Sam", "Java"))
